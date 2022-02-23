2e5be3b15c4ad8337e114774a1902be13b248a32
Gourav R <97089464+gouravr2231@users.noreply.github.com>
2022-02-23 04:55:19 +0000

    2

---
+++ p2original.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+void input_line(float *x1, float *y1)
+{
+   printf("enter the values of x,y\n");
+  scanf("%f%f",x1,y1);
+}
+int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3)
+{
+  int a,b,c;
+  a=sqrt(pow((x2-x1),2)+pow((y2-y1),2));
+  b=sqrt(pow((x3-x2),2)+pow((y3-y2),2));
+  c=sqrt(pow((x1-x3),2)+pow((y1-y3),2));
+  if((a+b)>c && (b+c)>a && (c+a)>b)
+    return 1;
+  else
+    return 0;
+}
+void output(float x1, float y1, float x2, float y2,float x3, float y3, int istriangle)
+{
+  if (istriangle == 1)
+    printf("the points (%f,%f),(%f,%f)&(%f,%f) form a triangle",x1,y1,x2,y2,x3,y3);
+  else 
+    printf("the points (%f,%f),(%f,%f)&(%f,%f) do not form a triangle\n",x1,y1,x2,y2,x3,y3);
+}
